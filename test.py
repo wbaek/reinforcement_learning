a6875ed17e83cffdd2c7b60c12c7b2887d5ecb6e
clint <clint.b@kakaobrain.com>
2017-10-20 18:02:19 +0900

    add testing

---
+++ test.py
@@ -0,0 +1,46 @@
+import logging
+import random
+import gym
+import tensorflow as tf
+
+from core.environment import Env
+from core.worker import Network, Worker
+
+
+def main(args):
+    logging.info( args )
+
+    env = gym.make(args.game)
+    env = Env(env, resized_width=84, resized_height=84, agent_history_length=4)
+    num_actions = len(env.gym_actions)
+
+    global_net = Network(num_actions, -1, 'cpu')
+
+    sess = tf.Session()
+    saver = tf.train.Saver()
+    saver.restore(sess, args.checkpoint)
+
+    state = env.get_initial_state()
+    terminal = False
+    while not terminal:
+        policy, value = global_net.predict(sess, state)
+        action_index = global_net.sampling(policy) 
+ 
+        new_state, reward, terminal = env.step(action_index)
+        env.env.render()
+
+if __name__ == '__main__':
+    import sys
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--game',    type=str,   default='Breakout-v0')
+    parser.add_argument('--checkpoint',    type=str,   required=True, help='./checkpoints/recent.ckpt')
+    parser.add_argument('--log-filename',  type=str,   default='')
+    args = parser.parse_args()
+
+    if not args.log_filename:
+        logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s %(levelname)s] %(message)s', stream=sys.stderr)
+    else:
+        logging.basicConfig(level=logging.INFO, format='[%(asctime)s %(levelname)s] %(message)s', filename=args.log_filename)
+
+    main( args )
